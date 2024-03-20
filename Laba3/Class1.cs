47bf2098b1bd976a342377b87f79918080ead1ea
Ambjorn772 <162361057+Ambjorn772@users.noreply.github.com>
2024-03-20 02:33:14 +0200

    Add files via upload

---
+++ Laba3/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba3
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Set set1 = new Set();
+            set1.Add(1);
+            set1.Add(2);
+            set1.Add(3);
+
+            Set set2 = new Set();
+            set2.Add(2);
+            set2.Add(3);
+            set2.Add(4);
+
+
+            Console.WriteLine(" 1: " + string.Join(", ", set1.GetElements()));
+            Console.WriteLine("Множина 2: " + string.Join(", ", set2.GetElements()));
+            Console.WriteLine("Множині 1 належить 2: " + set1.Contains(2));
+            Console.WriteLine("Множина є підмножиною Множини 2: " + set1.IsSubsetOf(set2));
+            Console.WriteLine("Множина 1 дорівнює Множині 2: " + set1.Equals(set2));
+
+            Set unionSet = set1.Union(set2);
+            Console.WriteLine("Об'єднання Множини 1 і Множини 2: " + string.Join(", ", unionSet.GetElements()));
+
+            Set intersectionSet = set1.Intersection(set2);
+            Console.WriteLine("Перетин Множини 1 і Множини 2: " + string.Join(", ", intersectionSet.GetElements()));
+
+            Set differenceSet = set1.Difference(set2);
+            Console.WriteLine("Різниця Множини 1 і Множини 2: " + string.Join(", ", differenceSet.GetElements()));
+
+            Set symmetricDifferenceSet = set1.SymmetricDifference(set2);
+            Console.WriteLine("Симетрична різниця Множини 1 і Множини 2: " + string.Join(", ", symmetricDifferenceSet.GetElements()));
+        }
+    }
+}
---
+++ Laba3/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Set
+{
+    private HashSet<int> elements;
+
+    public Set()
+    {
+        elements = new HashSet<int>();
+    }
+
+    public bool Contains(int element)
+    {
+        return elements.Contains(element);
+    }
+
+    public void Add(int element)
+    {
+        elements.Add(element);
+    }
+
+    public void Remove(int element)
+    {
+        elements.Remove(element);
+    }
+
+    public bool IsSubsetOf(Set otherSet)
+    {
+        return elements.IsSubsetOf(otherSet.elements);
+    }
+
+    public bool Equals(Set otherSet)
+    {
+        return elements.SetEquals(otherSet.elements);
+    }
+
+    public Set Union(Set otherSet)
+    {
+        Set unionSet = new Set();
+        foreach (int element in elements)
+        {
+            unionSet.Add(element);
+        }
+        foreach (int element in otherSet.elements)
+        {
+            unionSet.Add(element);
+        }
+        return unionSet;
+    }
+
+    public Set Intersection(Set otherSet)
+    {
+        Set intersectionSet = new Set();
+        foreach (int element in elements)
+        {
+            if (otherSet.Contains(element))
+            {
+                intersectionSet.Add(element);
+            }
+        }
+        return intersectionSet;
+    }
+
+    public Set Difference(Set otherSet)
+    {
+        Set differenceSet = new Set();
+        foreach (int element in elements)
+        {
+            if (!otherSet.Contains(element))
+            {
+                differenceSet.Add(element);
+            }
+        }
+        return differenceSet;
+    }
+
+    public Set SymmetricDifference(Set otherSet)
+    {
+        Set symmetricDifferenceSet = new Set();
+
+        foreach (int element in elements)
+        {
+            if (!otherSet.Contains(element))
+            {
+                symmetricDifferenceSet.Add(element);
+            }
+        }
+
+        foreach (int element in otherSet.elements)
+        {
+            if (!elements.Contains(element))
+            {
+                symmetricDifferenceSet.Add(element);
+            }
+        }
+
+        return symmetricDifferenceSet;
+    }
+
+    // Getter method for elements
+    public HashSet<int> GetElements()
+    {
+        return elements;
+    }
+}
+
+class Program
+{
+   
+}
